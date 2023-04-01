c9e7fc8a0a650ed24bdf12529b7d9ea441dfbc63
sohail-khan470 <sohailturk470@gmail.com>
2023-04-01 18:57:00 +0500

    Add Tags and Comments support

---
+++ backend/src/modules/comments/entities/comment.entity.ts
@@ -0,0 +1,44 @@
+// comment.entity.ts
+import { Task } from 'src/modules/tasks/entities/task.entity'
+import { User } from 'src/modules/users/user.entity'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Index
+} from 'typeorm'
+
+
+
+@Entity()
+@Index('idx_comments_task', ['taskId'])
+@Index('idx_comments_author', ['authorId'])
+@Index('idx_comments_created_at', ['createdAt'])
+export class Comment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ type: 'text' })
+  content: string
+
+  @Column({ nullable: true })
+  taskId: string
+
+  @ManyToOne(() => Task, task => task.comments)
+  task: Task
+
+  @Column({ nullable: true })
+  authorId: string
+
+  @ManyToOne(() => User)
+  author: User
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @DeleteDateColumn()
+  deletedAt: Date
+}
---
+++ backend/src/modules/tags/entities/tag.entity.ts
@@ -0,0 +1,24 @@
+// tag.entity.ts
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany
+} from 'typeorm'
+import { Task } from '../../tasks/entities/task.entity'
+
+
+@Entity()
+export class Tag {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ unique: true })
+  name: string
+
+  @Column({ type: 'varchar', length: 7, default: '#000000' })
+  color: string  // hex color (e.g., '#FF5733')
+
+  @ManyToMany(() => Task, task => task.tags)
+  tasks: Task[]
+}
