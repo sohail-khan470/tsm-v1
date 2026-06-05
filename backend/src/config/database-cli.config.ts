384f5444dadeb0e728133e364ea8714a8d427432
sohail-khan470 <sohailturk470@gmail.com>
2026-06-05 16:50:57 +0500

    fixed the compose file

--- backend/src/config/database-cli.config.ts
+++ backend/src/config/database-cli.config.ts
@@ -1,5 +1,7 @@
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
+import * as path from 'path';
+
 dotenv.config();
 
 export default new DataSource({
@@ -9,7 +11,7 @@ export default new DataSource({
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-    synchronize: process.env.NODE_ENV !== 'production',
-})
+    entities: [path.join(__dirname, '/../**/*.entity{.ts,.js}')],
+    migrations: [path.join(__dirname, '/database/migrations/*{.ts,.js}')],
+    synchronize: false, // Set to false when using migrations
+});
